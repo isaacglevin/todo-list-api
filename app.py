95ae0526070b8c9cef038f3ba58f866630ec1fca
ilevin1 <isaacglevin@gmail.com>
2025-12-31 21:08:39 -0500

    Update app.py to change default port to 8080 and disable debug mode and reloader

--- app.py
+++ app.py
@@ -113,6 +113,6 @@ class Todo(Resource):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
 
